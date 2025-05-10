a7fd132ccd7e1ff8bd9c37baab8f75a041632476
markcalendario <markcalendario@gmail.com>
2025-05-10 19:53:46 +0800

    feat: add react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          path="/"
+          element={<Home />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
