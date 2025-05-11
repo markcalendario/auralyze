3491febe5a7192b53f59e143d371454fc1ca03b1
markcalendario <markcalendario@gmail.com>
2025-05-11 23:18:08 +0800

    feat: connect client and server

--- client/src/pages/Auralyze/Auralyze.jsx
+++ client/src/pages/Auralyze/Auralyze.jsx
@@ -1,7 +1,8 @@
 import DropZone from "@/components/Forms/DropZone/DropZone.jsx";
 import TextArea from "@/components/Forms/TextArea/TextArea.jsx";
 import Navbar from "@/components/Navbar/Navbar.jsx";
-import { Fragment } from "react";
+import ResultLoader from "@/components/ResultLoader/ResultLoader.jsx";
+import { Fragment, useState } from "react";
 import styles from "./Auralyze.module.scss";
 
 export default function AuralyzePage() {
@@ -14,6 +15,41 @@ export default function AuralyzePage() {
 }
 
 function Auralyze() {
+  const [result, setResult] = useState(null);
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  const handleUploadAudio = async (evt) => {
+    setResult(null);
+    setIsProcessing(false);
+
+    const file = evt.target.files[0];
+
+    const formData = new FormData();
+    formData.append("audio", file);
+
+    setIsProcessing(true);
+
+    const url = import.meta.env.VITE_SERVER_URL + "/auralyze";
+    const config = { method: "POST", body: formData };
+    const request = await fetch(url, config);
+    const response = await request.json();
+
+    if (response.success) setResult(response.data);
+    else alert(response.message);
+
+    setIsProcessing(false);
+  };
+
+  const content =
+    isProcessing && result === null ? (
+      <ResultLoader />
+    ) : (
+      <Result
+        summary={result?.summary}
+        transcript={result?.transcript}
+      />
+    );
+
   return (
     <section className={styles.auralyze}>
       <div className={styles.container}>
@@ -21,10 +57,9 @@ function Auralyze() {
           <DropZone
             id="dropzone"
             accept="audio/*"
-            onChange={(evt) => console.log(evt.target.files)}
+            onChange={handleUploadAudio}
           />
-
-          <Result />
+          {content}
         </div>
       </div>
     </section>
