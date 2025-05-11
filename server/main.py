b50518fed3a8cc37c0611666e763311663afcebb
markcalendario <markcalendario@gmail.com>
2025-05-11 22:28:03 +0800

    feat: add cors

--- server/main.py
+++ server/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from config import Config
 from src.apis.auralyze.route import auralyze
 
 app = FastAPI()
 config = Config()
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=[config.CLIENT],  
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 app.include_router(auralyze, prefix="/auralyze")
